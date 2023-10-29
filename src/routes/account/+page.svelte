d586c8778bddbfee07d52967d0cee342c4f06bd4
bmehder <bmehder@gmail.com>
2023-10-29 15:19:57 -0400

    GitHub SSO

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -42,6 +42,18 @@
       message = 'Please check your email for a link to update your password.'
     }
   }
+
+  const handleSSOLogin = async () => {
+    err = null
+
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'github'
+    })
+
+    if (error) {
+      err = error
+    }
+  }
 </script>
 
 <h1 class="title">Login</h1>
@@ -78,6 +90,12 @@
 
 {#if !data.session && email}
   <button on:click={handlePasswordReset}>Forgot Password?</button>
+{/if}
+
+{#if !data.session}
+  <form>
+    <button on:click={handleSSOLogin}>GitHub</button>
+  </form>
 {/if}
 
 <style>
