c34e88a088677b69e5c1a5ab1f1edab501f6a23b
bmehder <bmehder@gmail.com>
2023-11-26 17:45:17 +0000

    backed off prefers color scheme

--- src/routes/top-secret/+page.svelte
+++ src/routes/top-secret/+page.svelte
@@ -4,8 +4,10 @@
 
 <h1 class="title">Top Secret 🕵️</h1>
 
-{#if data.session}
-  <p>The secret is 42</p>
-{:else}
-  <p>You can't handle the truth!</p>
-{/if}
+<section>
+  {#if data.session}
+    <p>The secret is 42</p>
+  {:else}
+    <p>You can't handle the truth!</p>
+  {/if}
+</section>
