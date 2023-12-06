0b29cf55f65607ac1a63ddec1830344111207411
bmehder <bmehder@gmail.com>
2023-12-06 16:56:07 +0000

    prerender false register route

--- src/routes/(admin)/register/+page.server.js
+++ src/routes/(admin)/register/+page.server.js
@@ -1,30 +1,32 @@
+export const prerender = false
+
 import { fail } from '@sveltejs/kit'
 
 export const actions = {
-  default: async ({ request, url, locals: { supabase } }) => {
-    const formData = await request.formData()
-    const email = formData.get('email')
-    const password = formData.get('password')
+	default: async ({ request, url, locals: { supabase } }) => {
+		const formData = await request.formData()
+		const email = formData.get('email')
+		const password = formData.get('password')
 
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${url.origin}/account/callback/`,
-      },
-    })
+		const { error } = await supabase.auth.signUp({
+			email,
+			password,
+			options: {
+				emailRedirectTo: `${url.origin}/account/callback/`,
+			},
+		})
 
-    if (error) {
-      return fail(500, {
-        message: 'Server error. Try again later.',
-        success: false,
-        email,
-      })
-    }
+		if (error) {
+			return fail(500, {
+				message: 'Server error. Try again later.',
+				success: false,
+				email,
+			})
+		}
 
-    return {
-      message: 'Please check your email for a magic link to log into the website.',
-      success: true,
-    }
-  },
+		return {
+			message: 'Please check your email for a magic link to log into the website.',
+			success: true,
+		}
+	},
 }
